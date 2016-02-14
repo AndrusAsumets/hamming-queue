afa41dda6489cd8c9c2630f11670cb5d17720ffb
Andrus Asumets <andrus.asumets@gmail.com>
2016-02-14 19:32:17 +0200

    JSON stringify fix

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ import { spotify } from './src/spotify';
 //web server
 let app = require('koa')();
 let router = require('koa-router')();
-let koaBody = require('koa-body')();
+let koaBody = require('koa-body');
+var serve = require('koa-static-server');
 
 app.use(router.routes());
+app.use(serve({ rootDir: 'data/images', rootPath: '/data/images' }));
+
 app.listen(process.env.PORT);
-console.log('Head is listening on', process.env.PORT);
+console.log('Queue is listening on', process.env.PORT);
 
 let spotifyOffset;
 try { spotifyOffset = JSON.parse(fs.readFileSync('./data/spotify.json')).offset
@@ -42,11 +45,12 @@ async function checkActionQueue() {
       spotifyOffset = spotifyOffset + ITEMS_TO_CRAWL;
 
       elements.forEach(element => {
-        const id = (new Date).getTime() + '_' + String(Math.random()).split('.')[1];
-        actionQueue.push({ job: element, id: id, host: host });
+        actionQueue.push({ type: 'spotify', job: element, id: getId(), host: host, priority: 0 });
       });
 
-      console.log('Successfully retrieved ' + elements.length + ' elements from Spotify. Offset is at ' + spotifyOffset + '.');
+      actionQueue.sort(function(a,b) { return b.priority < a.priority });
+
+      console.log('Successfully retrieved ' + elements.length + ' elements from Spotify. Offset is now at ' + spotifyOffset + '.');
     }
   }
 
@@ -63,34 +67,67 @@ function createActions() {
     const host = workers[i].host;
 
     let res;
-    try { res = syncRequest('POST', host + '/working') } catch(err) { continue };
+    try { res = syncRequest('POST', host + '/status') } catch(err) { continue };
 
     const body = JSON.parse(res.getBody('utf8'));
     const status = body.status;
 
     if (status == 'available') {
-      console.log('available')
+      console.log('Worker is available.')
 
-      request({ uri: host + '/job', method: 'POST', json: job }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          for (let j = 0; j < actionQueue.length; j++) {
-            if (actionQueue[j].id == job.id) {
-              actionQueue.splice(j, 1);
+      if (job.type == 'spotify') {
+        request({ uri: host + '/spotify', method: 'POST', json: job }, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            for (let j = 0; j < actionQueue.length; j++) {
+              if (actionQueue[j].id == job.id) {
+                actionQueue.splice(j, 1);
 
-              break;
+                break;
+              }
             }
           }
-        }
-      });
+        });
+      }
+      if (job.type == 'user') {
+        request({ uri: host + '/user', method: 'POST', json: job }, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            for (let j = 0; j < actionQueue.length; j++) {
+              if (actionQueue[j].id == job.id) {
+                actionQueue.splice(j, 1);
+
+                break;
+              }
+            }
+          }
+        });
+      }
     }
   }
 }
 setInterval(function() { createActions() }, 100);
 
-router.post('/complete', koaBody,
-  function *(next) {
+router.post('/complete', koaBody(),
+  function* (next) {
     console.log('/complete');
 
     this.body = JSON.stringify(this.request.body);
   }
 );
+
+router.post('/image', koaBody({
+    jsonLimit: '10mb'
+  }),
+  function* (next) {
+    console.log('/image');
+
+    var id = getId();
+    var path = '/data/images/' + id + '.jpg';
+
+    actionQueue.push({ type: 'user', job: host + path, id: id, host: host, priority: 10, data: this.request.body.image });
+
+    this.type = 'application/json';
+    this.body = JSON.stringify({ error: null });
+  }
+);
+
+function getId() { return (new Date).getTime() + '_' + String(Math.random()).split('.')[1] };
